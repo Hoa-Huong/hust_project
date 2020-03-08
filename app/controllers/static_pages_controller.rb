3b29ed3c05ba6ea24f0983d808201c89b39d038f
Hoa-Huong <hoahuongad@gmail.com>
2020-03-08 13:31:06 +0700

    Template adminlte3 dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def home
     @demands = Demand.includes(:province, :district).limit Settings.num_demand
-    @teachers = Teacher.includes(:user).limit Settings.num_teacher
+    @teachers = Teacher.includes(user: [:avatar_attachment]).limit Settings.num_teacher
   end
 end
---
+++ app/helpers/static_page_helper.rb
@@ -0,0 +1,21 @@
+module StaticPageHelper
+  def total_user
+    User.all.size
+  end
+
+  def total_new_offer
+    TeachOffer.pending.size
+  end
+
+  def total_new_demand
+    Demand.pending.size
+  end
+
+  def revenue
+    result = 0
+    Demand.approved.map do |demand|
+      result += demand.fee
+    end
+    result
+  end
+end
